f18e1e7f5651bf9e61b6b31562a52473bca6dbd0
Phuong Cao <caongocphuong95@gmail.com>
2023-02-23 20:55:14 +0700

    Interface segregation principle

---
+++ InterfaceSegregationPrinciple/Document.cs
@@ -0,0 +1,64 @@
+﻿namespace InterfaceSegregationPrinciple
+{
+    public class Document
+    {
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document document);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document document);
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionDevice : IPrinter, IScanner
+    {
+
+    }
+
+    public class MultiFunctionPrinter : IMultiFunctionDevice
+    {
+        private IPrinter _printer;
+        private IScanner _scanner;
+
+        public MultiFunctionPrinter(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            _printer = printer;
+            _scanner = scanner;
+        }
+
+        public void Print(Document document)
+        {
+            _printer.Print(document);
+        }
+
+        public void Scan(Document document)
+        {
+            _scanner.Scan(document);
+        }
+    }
+}
---
+++ InterfaceSegregationPrinciple/MultiFunctionDevice.cs
@@ -0,0 +1,7 @@
+﻿namespace InterfaceSegregationPrinciple
+{
+    public interface MultiFunctionDevice : IPrinter, IScanner
+    {
+
+    }
+}
